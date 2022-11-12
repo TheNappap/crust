0bbe35ff2242735f7e4d2ddf662cbf2590e7eb17
mathijsd <napoleonpapiertjes@gmail.com>
2022-11-12 15:48:15 +0100

    Refactoring: moved expression

--- src/parser/syntax_tree.rs
+++ src/parser/syntax_tree.rs
@@ -1,42 +1,14 @@
 
-use crate::lexer::{Literal};
 
-use self::{fn_expr::{Fn, Signature}, types::Type};
 use core::slice::{Iter, IterMut};
+
+use super::Fn;
 
 pub mod fn_expr;
 pub mod types;
+pub mod expression;
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum BinOpKind {
-    Add, Sub, Mul, Div, Eq, Neq
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum UnOpKind {
-    Neg
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum Expression {
-    Call(Signature, Vec<Expression>),
-    Fn(Fn),
-    Let(String, Box<Expression>, Type),
-    Mut(String, Box<Expression>),
-    If(Box<Expression>, Vec<Expression>, Option<Vec<Expression>>),
-    While(Box<Expression>, Vec<Expression>),
-    For(Box<Expression>, String, Type, Vec<Expression>),
-    Iter(Box<Expression>, u32),
-    Group(Vec<Expression>),
-    Literal(Literal),
-    AddrOf(Vec<Expression>),
-    Symbol(String, Type),
-    BinOp(BinOpKind, Box<Expression>, Box<Expression>, Type),
-    UnOp(UnOpKind, Box<Expression>, Type),
-    Return(Box<Expression>),
-    Array(Vec<Expression>),
-    Index(Box<Expression>, Box<Expression>, Type, u32),
-}
+pub use expression::{Expression, BinOpKind, UnOpKind};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct SyntaxTree {
---
+++ src/parser/syntax_tree/expression.rs
@@ -0,0 +1,32 @@
+use crate::{parser::{Signature, Fn, Type}, lexer::Literal};
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum BinOpKind {
+    Add, Sub, Mul, Div, Eq, Neq
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum UnOpKind {
+    Neg
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expression {
+    Call(Signature, Vec<Expression>),
+    Fn(Fn),
+    Let(String, Box<Expression>, Type),
+    Mut(String, Box<Expression>),
+    If(Box<Expression>, Vec<Expression>, Option<Vec<Expression>>),
+    While(Box<Expression>, Vec<Expression>),
+    For(Box<Expression>, String, Type, Vec<Expression>),
+    Iter(Box<Expression>, u32),
+    Group(Vec<Expression>),
+    Literal(Literal),
+    AddrOf(Vec<Expression>),
+    Symbol(String, Type),
+    BinOp(BinOpKind, Box<Expression>, Box<Expression>, Type),
+    UnOp(UnOpKind, Box<Expression>, Type),
+    Return(Box<Expression>),
+    Array(Vec<Expression>),
+    Index(Box<Expression>, Box<Expression>, Type, u32),
+}
